f2b66a743dfab20d91e1d2e54448eb9c33cfbcb2
Adam M <48637296+AdamM-1021@users.noreply.github.com>
2025-03-03 22:48:57 -0700

    Closes #62 (#75)

--- eis-tracker/app/login/page.tsx
+++ eis-tracker/app/login/page.tsx
@@ -2,15 +2,14 @@
 
 import React from "react";
 import Link from "next/link";
-import {useRouter} from "next/navigation";
 
 export default function Page() {
-    const router = useRouter();
 
-    const handleLogin = () => {
-        document.cookie = 'loggedIn=true; path=/';
-        router.push('/'); // Redirect to home after login
+
+    const handleLogin = async () => {
+        await fetch('/api/login', { method: 'POST' }); // Calls API to set the cookie
     };
+
 
     return (
       <div className="flex flex-col items-center justify-center min-h-screen p-8 sm:p-20 bg-gray-100">
@@ -29,6 +28,15 @@ export default function Page() {
                     Login
                   </button>
               </Link>
+              <Link href="/admin" passHref>
+                  <button
+                      className="px-6 py-3 bg-blue-500 text-white text-lg rounded-md hover:bg-blue-600 transition"
+                      onClick={handleLogin}
+                  >
+                      Admin
+                  </button>
+              </Link>
+
           </div>
       </div>
           );
