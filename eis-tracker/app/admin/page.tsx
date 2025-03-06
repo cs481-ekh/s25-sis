eee1c364db944989d16d433b06a99347b46740d5
Adam McCall <adam.max.mccall@gmail.com>
2025-03-06 09:12:47 -0700

    Adds Registration

--- eis-tracker/app/admin/page.tsx
+++ eis-tracker/app/admin/page.tsx
@@ -9,6 +9,45 @@ export default function Page() {
     const [blue, setBlue] = useState(false);
     const [green, setGreen] = useState(false);
     const [orange, setOrange] = useState(false);
+    const [admin, setAdmin] = useState(false);
+
+    const register = async () => {
+        let res = await fetch('/api/db', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ name , StudentID, mode: 'register' }),
+        });
+
+        if (res.ok) {
+            const data = await res.json();
+            console.log('Inserted User:', data.user);
+        } else {
+            console.error('Failed to insert user');
+        }
+        let tags = 0
+        if (white) {tags |= 0b1}
+        if (blue) {tags |= 0b10}
+        if (green) {tags |= 0b100}
+        if (orange) {tags |= 0b1000}
+        if (admin) {tags |= 0b10000}
+
+        res = await fetch('/api/db', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ tags, mode: 'edit_tags' }),
+        });
+
+        if (res.ok) {
+            console.log('Updated Tags To', tags);
+        } else {
+            console.error('Failed to update tags');
+        }
+    }
+
     return (
         <div className="flex flex-col items-start justify-start min-h-screen p-8 sm:p-20 bg-gray-100 space-y-4">
 
@@ -54,14 +93,23 @@ export default function Page() {
                     onChange={() => setOrange(!orange)}
                 /> Orange Tag
             </label>
+            <label>
+                <input
+                    type="checkbox"
+                    checked={admin}
+                    onChange={() => setAdmin(!admin)}
+                /> Admin User
+            </label>
             <div className="flex flex-col sm:flex-row gap-4 items-center">
                 <button
                     className="px-6 py-3 bg-blue-500 text-white text-lg rounded-md hover:bg-blue-600 transition"
+                    onClick={() => register()}
                 >
                     Register User
                 </button>
                 <button
                     className="px-6 py-3 bg-blue-500 text-white text-lg rounded-md hover:bg-blue-600 transition"
+                    onClick={() => register()}
                 >
                     Update User
                 </button>
