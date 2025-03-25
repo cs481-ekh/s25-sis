aa08d781c2baf13de5b307c91e93f09c368f701b
Adam M <48637296+AdamM-1021@users.noreply.github.com>
2025-03-25 10:25:32 -0600

    Implements admin login (#100)

--- eis-tracker/app/login/page.tsx
+++ eis-tracker/app/login/page.tsx
@@ -4,6 +4,7 @@ import React from "react";
 
 export default function Page() {
     const [id, setId] = React.useState<string>("");
+    const [pass, setPass] = React.useState<string>("");
 
     const handleLogin = async (e: React.FormEvent, link: string) => {
         e.preventDefault();
@@ -13,7 +14,7 @@ export default function Page() {
         const response = await fetch("/api/login", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ id }),
+            body: JSON.stringify({ id, pass }),
         });
 
         if (response.ok) {
@@ -25,34 +26,43 @@ export default function Page() {
 
 
     return (
-      <div className="flex flex-col items-center justify-center min-h-screen p-8 sm:p-20 bg-gray-100">
-          <h1 className="text-2xl font-bold mb-4">Enter Admin ID to login</h1>
-          <div className="flex flex-col sm:flex-row gap-4 items-center">
-              <input
-                  type="text"
-                  placeholder="Enter Admin ID"
-                  value={id}
-                  onChange={(e) => setId(e.target.value)}
-                  className="p-3 text-lg border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-              />
+        <div className="flex flex-col items-center justify-center min-h-screen p-8 sm:p-20 bg-gray-100">
+            <h1 className="text-2xl font-bold mb-4">Enter Admin ID and Password to Login</h1>
+            <div className="flex flex-col gap-4 items-center">
+                <input
+                    type="text"
+                    placeholder="Enter Admin ID"
+                    value={id}
+                    onChange={(e) => setId(e.target.value)}
+                    className="p-3 text-lg border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+                <input
+                    type="text"
+                    placeholder="Enter Admin Password"
+                    value={pass}
+                    onChange={(e) => setPass(e.target.value)}
+                    className="p-3 text-lg border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
 
-              <button
-                className="px-6 py-3 bg-blue-500 text-white text-lg rounded-md hover:bg-blue-600 transition"
-                onClick={(e) => handleLogin(e, "/")}
-              >
-                Login
-              </button>
+                <h3 className="text-2xl font-bold mb-4">Log in to:</h3>
 
+                <div className="flex flex-col sm:flex-row gap-4 items-center">
+                    <button
+                        className="px-6 py-3 bg-blue-500 text-white text-lg rounded-md hover:bg-blue-600 transition"
+                        onClick={(e) => handleLogin(e, "/")}
+                    >
+                        Home
+                    </button>
 
-              <button
-                  className="px-6 py-3 bg-blue-500 text-white text-lg rounded-md hover:bg-blue-600 transition"
-                  onClick={(e) => handleLogin(e, "/admin")}
-              >
-                  Admin
-              </button>
+                    <button
+                        className="px-6 py-3 bg-blue-500 text-white text-lg rounded-md hover:bg-blue-600 transition"
+                        onClick={(e) => handleLogin(e, "/admin")}
+                    >
+                        Admin
+                    </button>
+                </div>
+            </div>
+        </div>
 
-
-          </div>
-      </div>
-          );
+    );
 }
